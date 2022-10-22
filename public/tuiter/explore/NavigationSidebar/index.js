4d3056824b8ae5e69811463dcd5c014df237bdf4
AvRajath <rajathav28@gmail.com>
2022-10-22 02:16:58 -0400

    a5 javascript fall 2022

---
+++ public/tuiter/explore/NavigationSidebar/index.js
@@ -0,0 +1,50 @@
+const NavigationSidebar = (page) => {
+    return(`
+   <div class="list-group d-none d-lg-block">
+     <a class="list-group-item" href="#">
+        <i class="fab fa-twitter text-white"></i><br/></a>       
+     <a href="../home.html" class="list-group-item ${page === 'home' ? 'active' : '' }">
+        <i class="fa fa-home text-white"> </i> Home</a>
+     <a href="index.html" class="list-group-item ${page === 'explore' ? 'active' : '' }">
+        <i class="fa fa-hashtag fw-bold"> </i> Explore </a>
+     <a href="#" class="list-group-item">
+        <i class="fa fa-bell text-white fw-normal"> </i> Notifications</a>
+     <a href="#" class="list-group-item ">
+        <i class="fas fa-envelope text-white fw-normal"> </i> Messages</a>
+     <a href="../bookmarks/index.html" class="list-group-item">
+        <i class="fas fa-bookmark text-white fw-normal"></i> Bookmarks</a>
+     <a href="#" class="list-group-item ">
+        <i class="fas fa-list text-white "> </i> Lists</a>
+     <a href="../profile.html" class="list-group-item">
+        <i class="fas fa-user text-white"> </i> Profile</a>
+     <a href="#" class="list-group-item">
+        <i class="fas fa-ellipsis-h text-white"> </i> More</a>
+   </div>
+   <div class="list-group d-lg-none">
+     <a class="list-group-item" href="#">
+        <i class="fab fa-twitter text-white"></i><br/></a>       
+     <a href="../home.html" class="list-group-item ${page === 'home' ? 'active' : '' }">
+        <i class="fa fa-home text-white"> </i></a>
+     <a href="index.html" class="list-group-item ${page === 'explore' ? 'active' : '' }">
+        <i class="fa fa-hashtag fw-bold"> </i> </a>
+     <a href="#" class="list-group-item">
+        <i class="fa fa-bell text-white fw-normal"> </i></a>
+     <a href="#" class="list-group-item ">
+        <i class="fas fa-envelope text-white fw-normal"> </i></a>
+     <a href="../bookmarks/index.html" class="list-group-item">
+        <i class="fas fa-bookmark text-white fw-normal"></i></a>
+     <a href="#" class="list-group-item ">
+        <i class="fas fa-list text-white "> </i></a>
+     <a href="../profile.html" class="list-group-item">
+        <i class="fas fa-user text-white"> </i></a>
+     <a href="#" class="list-group-item">
+        <i class="fas fa-ellipsis-h text-white"> </i></a>
+   </div>
+   <div class="d-grid mt-2">
+     <a href="tweet.html"
+        class="btn btn-primary btn-block rounded-pill">
+        Tuit</a>
+   </div>
+ `);
+}
+export default NavigationSidebar;
---
+++ public/tuiter/explore/PostSummaryList/PostSummaryItem.js
@@ -0,0 +1,25 @@
+const PostSummaryItem = (post) => {
+    return(`
+        <li class="list-group-item">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-9">
+                        <div class="text-secondary wd-text-size">
+                            ${post.topic}
+                        </div>
+                        <p class="card-text text-white"><b> 
+                            ${post.userName} <i class="bi bi-patch-check text-secondary"> - ${post.time} </i></b></p>
+                        <p class="card-text text-white"><b>${post.title}</b></p>
+                        <div class="text-secondary wd-text-size">
+                            ${post.tweets}
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <img src=${post.image} class="rounded wd-img-main" alt="Cinque Terre"/>
+                    </div>
+                </div>
+            </div>
+        </li>
+    `);
+}
+export default PostSummaryItem;
