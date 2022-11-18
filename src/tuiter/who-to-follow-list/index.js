03f4cbb080f0b532bc7d40b7399e7e9b5fd895d4
AvRajath <rajathav28@gmail.com>
2022-11-18 03:23:00 -0500

    Updated who to follow

--- src/tuiter/who-to-follow-list/index.js
+++ src/tuiter/who-to-follow-list/index.js
@@ -3,11 +3,20 @@ import WhoToFollowListItem from "./who-to-follow-list-item.js";
 import { useSelector } from "react-redux";
 
 const WhoToFollowList = () => {
-  const whoArray = useSelector(state => state.who);
+  const who = useSelector(state => state.who);
   return (
-    <div>
-      <h1>Who To Follow!!</h1>
-    </div>
+    <ul className="list-group">
+      <li className="list-group-item">
+        <div className="row">
+          <span className="fs-3">Who to follow</span>
+        </div>
+      </li>
+      {who.map((who, index) => (
+        <div key={index}>
+          <WhoToFollowListItem who={who} key={index} />
+        </div>
+      ))}
+    </ul>
   );
 };
 
